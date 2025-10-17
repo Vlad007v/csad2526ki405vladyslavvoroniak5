1e8087cdda135a12afdbd35886e0a3f7d85a7af8
Vlad007v <voroniak2005@gmail.com>
2025-10-17 15:08:00 +0300

    commit unit testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "math_operations.h"
 
 int main() {
     std::cout << "Hello, world!" << std::endl;
+    int a = 3;
+    int b = 4;
+    std::cout << "add(" << a << ", " << b << ") = " << add(a, b) << std::endl;
     return 0;
 }
